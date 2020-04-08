510fe877a7e4e9fc56ff279c5bdeb49d7b626d8c
emech <mh.niroomand91@gmail.com>
2020-04-08 12:21:25 +0430

    fix: remove ApiProperty definitions from UserBaseEntity

--- src/authentication/models/user.base.entity.ts
+++ src/authentication/models/user.base.entity.ts
@@ -1,41 +1,30 @@
 import { Column, Entity, TableInheritance } from 'typeorm';
-import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { AbstractEntity } from '../../models';
-import { Exclude } from 'class-transformer';
 
 @Entity('user')
 @TableInheritance({ column: { name: 'userType', type: 'varchar' } })
 export class UserBaseEntity extends AbstractEntity {
-  @ApiPropertyOptional({ uniqueItems: true, readOnly: true })
   @Column({ unique: true, nullable: true })
   username?: string;
 
-  @ApiPropertyOptional({ format: 'email', uniqueItems: true, readOnly: true })
   @Column({ unique: true, nullable: true })
   email?: string;
 
-  @ApiPropertyOptional({ uniqueItems: true, readOnly: true })
   @Column({ unique: true, nullable: true })
   phone?: string;
 
-  @ApiPropertyOptional({ uniqueItems: true, readOnly: true })
   @Column({ unique: true, nullable: true })
   facebookId?: string;
 
-  @ApiPropertyOptional({ uniqueItems: true, readOnly: true })
   @Column({ unique: true, nullable: true })
   xingId?: string;
 
-  @Exclude()
   @Column({ nullable: true })
   password?: string;
 
-  @ApiPropertyOptional({ readOnly: true })
   @Column({ nullable: true })
-  @Exclude({})
   shouldChangePassword?: boolean;
 
-  @ApiProperty({ readOnly: true })
   @Column({ default: false })
   isBanned: boolean;
 
